26e70ceb75aaafb0b4138e0ebb084e3481036c82
genericarati <genericarati@gmail.com>
2018-02-10 21:12:23 -0500

    Page with trade button and text

---
+++ events-app/src/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h1, img, button)
+import Html.Attributes as Attribute exposing (..)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [ myStyle ]
+            [ text "I am dealer 1" ]
+        , div []
+            [ button [ myStyle ] [ text "Request trade" ]
+            ]
+        ]
+
+
+myStyle : Html.Attribute msg
+myStyle =
+    style
+        [ ( "margin-left", "auto" )
+        , ( "margin-right", "auto" )
+        , ( "margin-top", "20px" )
+        , ( "width", "100px" )
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
