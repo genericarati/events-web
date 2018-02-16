ac01a6265c97da6c515f5317da073611e9060ac7
genericarati <genericarati@gmail.com>
2018-02-15 20:12:10 -0500

    Getting valid json from backend

--- events-app/src/Main.elm
+++ events-app/src/Main.elm
@@ -3,52 +3,82 @@ module Main exposing (..)
 import Html exposing (Html, text, div, h1, img, button)
 import Html.Attributes as Attribute exposing (..)
 import Html.Events exposing (onClick)
-
-
----- MODEL ----
+import RemoteData.Http
+import Json.Decode
+import RemoteData exposing (WebData, RemoteData(..))
+import Json.Decode.Pipeline
 
 
 type alias Model =
-    { order : String, dealer : String }
+    { order : String, dealer : String, response : WebData Order }
+
+
+type alias Order =
+    { message : String }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { order = "12345", dealer = "dealer1" }, Cmd.none )
-
-
-
----- UPDATE ----
+    ( { order = "12345", dealer = "dealer1", response = NotAsked }, Cmd.none )
 
 
 type Msg
     = NoOp
     | RequestTrade
+    | TradeResponse (WebData Order)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RequestTrade ->
-            ( { model | dealer = "dealer2" }, Cmd.none )
+            ( model, createGetRequest )
+
+        TradeResponse response ->
+            ( { model | response = response }, Cmd.none )
 
         _ ->
-            ( model, Cmd.none )
-
-
-
----- VIEW ----
+            let
+                _ =
+                    Debug.log "all other cases"
+            in
+                ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [ myStyle ]
-            [ text "I am dealer 1", text model.dealer ]
-        , div []
-            [ button [ myStyle, onClick RequestTrade ] [ text "Request trade" ]
+    let
+        value =
+            case model.response of
+                Success data ->
+                    data
+
+                NotAsked ->
+                    { message = "Initial value" }
+
+                _ ->
+                    { message = "Problem" }
+    in
+        div []
+            [ div [ myStyle ]
+                [ text "I am dealer 1-", text value.message ]
+            , div []
+                [ button [ myStyle, onClick RequestTrade ] [ text "Request trade" ]
+                ]
             ]
-        ]
+
+
+tradeResponseDecoder : Json.Decode.Decoder Order
+tradeResponseDecoder =
+    Json.Decode.Pipeline.required
+        "message"
+        Json.Decode.string
+        (Json.Decode.Pipeline.decode Order)
+
+
+createGetRequest : Cmd Msg
+createGetRequest =
+    RemoteData.Http.get "http://localhost:8080/" TradeResponse tradeResponseDecoder
 
 
 myStyle : Html.Attribute msg
@@ -59,10 +89,6 @@ myStyle =
         , ( "margin-top", "20px" )
         , ( "width", "100px" )
         ]
-
-
-
----- PROGRAM ----
 
 
 main : Program Never Model Msg
