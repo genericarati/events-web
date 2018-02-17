b457591b6fecb49acd3fa8f4f7c703f918d03c0a
genericarati <genericarati@gmail.com>
2018-02-16 21:32:47 -0500

    Trying to create single page elm app

--- events-app/src/Main.elm
+++ events-app/src/Main.elm
@@ -10,8 +10,13 @@ import Json.Decode.Pipeline
 import Navigation exposing (Location)
 
 
+type Page
+    = Dealer1
+    | Dealer2
+
+
 type alias Model =
-    { response : WebData Order }
+    { response : WebData Order, currentPage : Page }
 
 
 type alias Order =
@@ -20,7 +25,7 @@ type alias Order =
 
 init : Location -> ( Model, Cmd Msg )
 init location =
-    ( { response = NotAsked }, Cmd.none )
+    ( Model NotAsked Dealer1, createGetRequest "dealer1" )
 
 
 type Msg
@@ -28,27 +33,48 @@ type Msg
     | RequestTrade
     | TradeResponse (WebData Order)
     | UrlChange Location
+    | Dealer1Page
+    | Dealer2Page
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RequestTrade ->
-            ( model, createGetRequest )
+            ( model, createGetRequest "dealer2" )
 
         TradeResponse response ->
             ( { model | response = response }, Cmd.none )
 
-        _ ->
-            let
-                _ =
-                    Debug.log "all other cases"
-            in
-                ( model, Cmd.none )
+        Dealer1Page ->
+            ( { model | currentPage = Dealer1 }, createGetRequest "dealer1" )
+
+        Dealer2Page ->
+            ( { model | currentPage = Dealer2 }, createGetRequest "dealer2" )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+        UrlChange location ->
+            ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ div [ myStyle ]
+            [ render_page model
+            , render_menu model
+            ]
+        , div []
+            [ button [ myStyle, onClick RequestTrade ]
+                [ text "Request trade" ]
+            ]
+        ]
+
+
+render_page : Model -> Html Msg
+render_page model =
     let
         value =
             case model.response of
@@ -60,14 +86,36 @@ view model =
 
                 _ ->
                     { ordernumber = "", dealer = "" }
+
+        page_content =
+            case model.currentPage of
+                Dealer1 ->
+                    div []
+                        [ div []
+                            [ div [] [ text "I am dealer1" ]
+                            , div [] [ text value.ordernumber ]
+                            , div [] [ text value.dealer ]
+                            ]
+                        ]
+
+                Dealer2 ->
+                    div []
+                        [ div []
+                            [ div [] [ text "I am dealer2" ]
+                            , div [] [ text value.ordernumber ]
+                            , div [] [ text value.dealer ]
+                            ]
+                        ]
     in
-        div []
-            [ div [ myStyle ]
-                [ text value.dealer ]
-            , div []
-                [ button [ myStyle, onClick RequestTrade ] [ text "Request trade" ]
-                ]
-            ]
+        div [] [ page_content ]
+
+
+render_menu : Model -> Html Msg
+render_menu model =
+    div []
+        [ button [ onClick Dealer1Page ] [ text "Dealer1 Page" ]
+        , button [ onClick Dealer2Page ] [ text "Dealer2 Page" ]
+        ]
 
 
 tradeResponseDecoder : Json.Decode.Decoder Order
@@ -77,9 +125,9 @@ tradeResponseDecoder =
         |> Json.Decode.Pipeline.required "dealer" Json.Decode.string
 
 
-createGetRequest : Cmd Msg
-createGetRequest =
-    RemoteData.Http.get "http://localhost:8080/" TradeResponse tradeResponseDecoder
+createGetRequest : String -> Cmd Msg
+createGetRequest dealerId =
+    RemoteData.Http.get ("http://localhost:8080/" ++ dealerId) TradeResponse tradeResponseDecoder
 
 
 myStyle : Html.Attribute msg
