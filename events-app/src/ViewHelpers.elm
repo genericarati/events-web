92402e7976ccad6aadd3134d4d63d24267c069af
genericarati <genericarati@gmail.com>
2018-02-18 22:52:37 -0500

    notification

--- events-app/src/ViewHelpers.elm
+++ events-app/src/ViewHelpers.elm
@@ -7,11 +7,13 @@ import Msg exposing (..)
 import GetOrders exposing (..)
 import Material.Table as Table exposing (..)
 import Html.Events exposing (onClick)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 myStyle : Html.Attribute msg
 myStyle =
-    style
+    Attributes.style
         [ ( "margin-left", "auto" )
         , ( "margin-right", "auto" )
         , ( "margin-top", "20px" )
@@ -24,9 +26,9 @@ render_order_table orderList model =
     Table.table []
         [ Table.thead []
             [ Table.tr []
-                [ Table.th [] [ text "Order" ]
-                , Table.th [] [ text "Dealer" ]
-                , Table.th [] [ text "Action" ]
+                [ Table.th [] [ Html.text "Order" ]
+                , Table.th [] [ Html.text "Dealer" ]
+                , Table.th [] [ Html.text "Action" ]
                 ]
             ]
         , Table.tbody []
@@ -34,12 +36,12 @@ render_order_table orderList model =
                 |> List.map
                     (\order ->
                         Table.tr []
-                            [ Table.td [] [ text order.ordernumber ]
-                            , Table.td [ Table.numeric ] [ text order.dealer ]
+                            [ Table.td [] [ Html.text order.ordernumber ]
+                            , Table.td [ Table.numeric ] [ Html.text order.dealer ]
                             , Table.td [ Table.numeric ]
                                 [ Html.button
                                     [ Html.Events.onClick (RequestTrade order) ]
-                                    [ text "Request Trade" ]
+                                    [ Html.text "Request Trade" ]
                                 ]
                             ]
                     )
@@ -57,14 +59,23 @@ render_page model =
             case model.currentPage of
                 Dealer1 ->
                     Html.div [ myStyle ]
-                        [ h1 [] [ text "I am dealer1" ]
+                        [ h1 [] [ Html.text "I am dealer1" ]
+                        , notificationIcon
                         , render_order_table orderList model
                         ]
 
                 Dealer2 ->
                     Html.div [ myStyle ]
-                        [ h1 [] [ text "I am dealer2" ]
+                        [ h1 [] [ Html.text "I am dealer2" ]
+                        , div [] [ notificationIcon ]
                         , render_order_table orderList model
                         ]
     in
         Html.div [] [ page_content ]
+
+
+notificationIcon : Html.Html msg
+notificationIcon =
+    svg
+        [ Svg.Attributes.width "24", Svg.Attributes.height "24", viewBox "0 0 24 24", Svg.Attributes.fill "#000000" ]
+        [ Svg.path [ d "M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" ] [] ]
